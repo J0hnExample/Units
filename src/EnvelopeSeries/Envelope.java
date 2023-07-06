097548e1c4336d4effe42db7a73d44863bdbf671
s67427 <s67427@bht-berlin.de>
2023-07-06 17:48:16 +0200

    Updated envelopeseries

--- src/EnvelopeSeries/Envelope.java
+++ src/EnvelopeSeries/Envelope.java
@@ -1,14 +1,25 @@
 package EnvelopeSeries;
-
+import java.util.Arrays;
 public class Envelope {
-    public static int width = 50;
+
+    // Envelope Parameter (with "Um-Eins-daneben-Fehler")
+    public static int width = 50; // Envelope witdh
     public static int border = 1;
-    public static int padding = 3;
-    public static String spaceChar = " ";
+    public static int padding = 3; // Space between border and Text
+
+    // Chars used to create the Envelope
+    public static String spaceChar = " "; // Used for free spaces
     public static String borderHorizontalChar = "-";
     public static String borderVerticalChar = "|";
-    public static String cornerChar = "+";
-    public static String lineBreak = "\n";
+    public static String cornerChar = "+"; // One in each Corner
+    public static String lineBreak = "\n"; // Helping Char to get in new line
+    // Default Adresses and String Array initialisation
+    public static String fromName = "$NAME", fromAddress = "$ADDRESS", fromZipCodeAndCity = "$ZIP $CITY";
+    public static String[] fromAddresses = { fromName, fromAddress, fromZipCodeAndCity };
+
+    public static String toName = "$NAME", toAddress = "$ADDRESS", toZipCodeAndCity = "$ZIP $CITY";
+    public static String[] toAddresses = { toName, toAddress, toZipCodeAndCity };
+
 
 
 public void printEnvelope(Student[] toStudents) {
@@ -26,243 +37,78 @@ public void printEnvelope(Student[] toStudents) {
 }
     public static void printEnvelope(String fromName, String fromAddress, String fromZipCodeAndCity, String toName,
             String toAddress, String toZipCodeAndCity) {
-
-        String[] adresses = { toName, toAddress, toZipCodeAndCity };
-        int spacing = theLongestwithPadding(adresses);
-
-        lineClose(border);
-        lineEmpty();
-        lineDummySendName(fromName);
-        lineDummySendAdress(fromAddress);
-        lineDummySendZipCity(fromZipCodeAndCity);
-        lineEmpty();
-        lineDummyRecName(toName, spacing);
-        lineDummyRecAdress(toAddress, spacing);
-        lineDummyRecZipCity(toZipCodeAndCity, spacing);
-        lineEmpty();
-        lineClose(border);
+                    fromAddresses[0] =fromName;
+                    fromAddresses[1] =fromAddress;
+                    fromAddresses[2] =fromZipCodeAndCity;
+                    toAddresses[0] =toName;
+                    toAddresses[1] =toAddress;
+                    toAddresses[2] =toZipCodeAndCity;
+        //Calculate the Space to textfield of the right side of the envelope(to Addressfield)
+        int spacing = theLongestwithPadding(toAddresses);
+    //Creating the Envelope line by line
+        line(borderHorizontalChar, cornerChar);
+        line(spaceChar, borderVerticalChar);
+        lineAddress(fromAddresses, padding, spacing, "from");
+        line(spaceChar, borderVerticalChar);
+        lineAddress(toAddresses, padding, spacing, "to");
+        line(spaceChar, borderVerticalChar);
+        line(borderHorizontalChar, cornerChar);
     }
-    
-    public static void lineEmpty() {
-        // Print empty line
-        for (int i = 0; i <= width-1; i++) {
-            if (i == 0 || i == width-1) {
-                System.out.print(borderVerticalChar);
-                if (i == width-1) {
+    //Creating a line withoud address
+    public static void line(String end, String middle) {
+        //end prints the left and right endchar and middle the char between
+        for (int i = 0; i <= width - 1; i++) {
+            if (i == 0 || i == width - 1) {
+                System.out.print(middle);
+                if (i == width - 1) {
                     for (int j = 0; j < border; j++) {
                         System.out.print(" ");
                     }
                     System.out.print("\n");
                 }
             } else {
-                System.out.print(spaceChar);
+                System.out.print(end);
             }
         }
 
     }
-
-    public static void lineClose(int border) {
-        // line
-        for (int i = 0; i <= width-1; i++) {
-            if (i == 0 || i == width-1) {
-                System.out.print(cornerChar);
-                if (i == width-1) {
-                    for (int j = 0; j < border; j++) {
-                        System.out.print(" ");
+    //Printing addresslines linetype "to" and "frome" decide padding or spacing to the side
+    public static void lineAddress(String[] addresses, int padding, int spacing, String lineType) {
+        for (int line = 0; line < addresses.length; line++) {       //Taking the adressline count
+            String address = addresses[line];                       //Pullout one adress line of the array and put it in a single string
+            for (int i = 0; i <= width - 1; i++) {                  //preventing "Obi-Wan error" while printing the line witdh
+                if (i == 0 || i == width - 1) {
+                    System.out.print(borderVerticalChar);           //Printing the space 
+                    if (i == width - 1) {                           //When on the side(start or end of line) printing Border if needed
+                        for (int j = 0; j < border; j++) {
+                            System.out.print(" ");
+                        }
+                        System.out.print("\n");                     //Goto next line
                     }
-                    System.out.print("\n");
-                }
-            } else {
-                System.out.print(borderHorizontalChar);
-            }
-        }
-    }
-
-    public static void lineDummySendName(String name) {
-
-        for (int i = 0; i <= width-1; i++) {
-            if (i == 0 || i == width-1) {
-                System.out.print(borderVerticalChar);
-                if (i == width-1) {
-                    for (int j = 0; j < border; j++) {
-                        System.out.print(" ");
-                    }
-                    System.out.print("\n");
-                }
-
-            }
-            if (i == padding) {
-                System.out.print(name);
-                i += name.length();
-
-            } else {
-                if (i == width-1) {
-                    continue;
+                } // Creating Adresses - first choose between "to" and "from" address and using padding or spacing
+                if ((lineType.equals("to") && i == spacing) || (lineType.equals("from") && i == padding)) { 
+                    System.out.print(address);                      //prints the address
+                    i += address.length();                          //offset the i by the length of the printed string
                 } else {
-                    System.out.print(spaceChar);
-                }
-
-            }
-
-        }
-
-    }
-
-    public static void lineDummySendAdress(String address) {
-        for (int i = 0; i <= width-1; i++) {
-            if (i == 0 || i == width-1) {
-                System.out.print(borderVerticalChar);
-                if (i == width-1) {
-                    for (int j = 0; j < border; j++) {
-                        System.out.print(" ");
+                    if (i == width - 1) {                           //if the line is ready printed leave
+                        continue;
+                    } else {                                        //if the line is not ready printed print spaces
+                        System.out.print(spaceChar);
                     }
-                    System.out.print("\n");
                 }
             }
-            if (i == padding) {
-                System.out.print(address);
-                i += address.length();
-
-            } else {
-                if (i == width-1) {
-                    continue;
-                } else {
-                    System.out.print(spaceChar);
-                }
-
-            }
-
-        }
-
-    }
-
-    public static void lineDummySendZipCity(String zipCity) {
-        for (int i = 0; i <= width-1; i++) {
-            if (i == 0 || i == width-1) {
-                System.out.print(borderVerticalChar);
-                if (i == width-1) {
-                    for (int j = 0; j < border; j++) {
-                        System.out.print(" ");
-                    }
-                    System.out.print("\n");
-                }
-            }
-            if (i == padding) {
-                System.out.print(zipCity);
-                i += zipCity.length();
-
-            } else {
-                if (i == width-1) {
-                    continue;
-                } else {
-                    System.out.print(spaceChar);
-                }
-
-            }
-
-        }
-
-    }
-
-    public static void lineDummyRecName(String toName, int spacing) {
-
-        for (int i = 0; i <= width-1; i++) {
-            if (i == 0 || i == width-1) {
-                System.out.print(borderVerticalChar);
-                if (i == width-1) {
-                    for (int j = 0; j < border; j++) {
-                        System.out.print(" ");
-                    }
-                    System.out.print("\n");
-                }
-
-            }
-            if (i == spacing) {
-                System.out.print(toName);
-                i += toName.length();
-
-            } else {
-                if (i == width-1) {
-                    continue;
-                } else {
-                    System.out.print(spaceChar);
-                }
-
-            }
-
-        }
-
-    }
-
-    public static void lineDummyRecAdress(String toAddress, int spacing) {
-        for (int i = 0; i <= width-1; i++) {
-            if (i == 0 || i == width-1) {
-                System.out.print(borderVerticalChar);
-                if (i == width-1) {
-                    for (int j = 0; j < border; j++) {
-                        System.out.print(" ");
-                    }
-                    System.out.print("\n");
-                }
-            }
-            if (i == spacing) {
-                System.out.print(toAddress);
-                i += toAddress.length();
-
-            } else {
-                if (i == width-1) {
-                    continue;
-                } else {
-                    System.out.print(spaceChar);
-                }
-
-            }
-
-        }
-
-    }
-
-    public static void lineDummyRecZipCity(String toZipCodeAndCity, int spacing) {
-        for (int i = 0; i <= width-1; i++) {
-            if (i == 0 || i == width-1) {
-                System.out.print(borderVerticalChar);
-                if (i == width-1) {
-                    for (int j = 0; j < border; j++) {
-                        System.out.print(" ");
-                    }
-                    System.out.print("\n");
-                }
-            }
-            if (i == spacing) {
-                System.out.print(toZipCodeAndCity);
-                i += toZipCodeAndCity.length();
-
-            } else {
-                if (i == width-1) {
-                    continue;
-                } else {
-                    System.out.print(spaceChar);
-                }
-
-            }
-
         }
     }
-
+    //Methode calculates the longest sting of the address and adds padding by using the Array.stream methode
     public static int theLongestwithPadding(String[] strings) {
-        // Calculates the longest string and creating text space with the padding
-        int maxLength = 0;
-
-        for (String s : strings) {
-            if (s.length() > maxLength) {
-                maxLength = s.length();
-
-            }
-        }
-        int param = maxLength + padding;
-        int spacing = width-1 - param - 1;
-
+        int spacing = width - 1 - (Arrays.stream(strings).mapToInt(String::length).max().orElse(0) + padding) - 1;
         return spacing;
     }
-
 }
+
+/*Changelog 325 Lines reduced to 125 Lines
+- Creating Address Arrays
+- combined methodes for printing empty-lines and end-lines
+- combined methodes for "to" and "from" addressprinting
+- reduced thelongestwithpadding methode by Array.stream magic
+*/
