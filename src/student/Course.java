7f7818e8064e9e01b9dd296e23e09ce3c704c4b4
SaschaAthmer <33932092+J0hnExample@users.noreply.github.com>
2023-07-10 21:55:38 +0200

    Package

--- src/student/Course.java
+++ src/student/Course.java
@@ -1,5 +1,27 @@
 package student;
 
-public record Course(String name, String teacher) {
-    
+public class Course {
+    private String name;
+    private String teacher;
+
+    public Course(String name, String teacher) {
+        this.name = name;
+        this.teacher = teacher;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
 }
--- src/student/Student.java
+++ src/student/Student.java
@@ -1,15 +1,141 @@
 package student;
 
-public record  Student(String prename, String surname, String street, String houseNumber, String zipCode, String city, String registrationNumber, StudyCourse studyCourse, int age) {
+public class Student {
+    private String prename;
+    private String surname;
+    private String street;
+    private String houseNumber;
+    private String zipCode;
+    private String city;
+    private String registrationNumber;
+    private StudyCourse studyCourse;
+    private int age;
+    private Course[] bookedCourses;
 
-                public Student(String prename, String surname, String street, String houseNumber, String zipCode, String city, String registrationNumber)
-                {
-                    this(prename, surname, street, houseNumber, zipCode, city, registrationNumber, 20);
+    public Student(String prename, String surname, String street, String houseNumber, String zipCode, String city,
+            String registrationNumber) {
+        this(prename, surname, street, houseNumber, zipCode, city, registrationNumber, StudyCourse.MEDIENINFORMATIK, 20);
+        bookedCourses = new Course[5];
+    }
 
-                }  
-                public Student(String prename, String surname, String street, String houseNumber, String zipCode, String city, String registrationNumber, int age){
-                    this(prename, surname, street, houseNumber, zipCode, city, registrationNumber,MEDIENINFORMATIK, age);
-                }  
+    public Student(String prename, String surname, String street, String houseNumber, String zipCode, String city,
+            String registrationNumber, int age) {
+        this(prename, surname, street, houseNumber, zipCode, city, registrationNumber, StudyCourse.MEDIENINFORMATIK,
+                age);
+        bookedCourses = new Course[5];
+    }
+
+    public Student(String prename, String surname, String street, String houseNumber, String zipCode, String city,
+            String registrationNumber, StudyCourse studyCourse, int age) {
+        this.prename = prename;
+        this.surname = surname;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.registrationNumber = registrationNumber;
+        this.studyCourse = studyCourse;
+        this.age = age;
+        bookedCourses = new Course[5];
+    }
+
+    public void bookCourse(Course course) {
+        for (int i = 0; i < bookedCourses.length; i++) {
+            if (bookedCourses[i] == null) {
+                bookedCourses[i] = course;
+                break;
             }
+        }
+    }
 
-  
+    public int getBookedCoursesCount() {
+        int count = 0;
+        for (Course course : bookedCourses) {
+            if (course != null) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String[] getNameAndAddress() {
+        return new String[] { prename, surname, street, houseNumber, zipCode, city };
+    }
+
+    @Override
+    public String toString() {
+        return prename + " " + surname + " (" + registrationNumber + "), " + studyCourse;
+    }
+
+    public String getPrename() {
+        return prename;
+    }
+
+    public void setPrename(String prename) {
+        this.prename = prename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public StudyCourse getStudyCourse() {
+        return studyCourse;
+    }
+
+    public void setStudyCourse(StudyCourse studyCourse) {
+        this.studyCourse = studyCourse;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
